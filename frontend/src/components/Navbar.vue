e205c0e92bc12adbe18d074810d162776f1c92de
HBilal Khan Yousafzai <hbilal_9@hotmail.com>
2025-03-21 17:40:17 +0500

    feat(navbar): add Navbar component and integrate into App.vue 🎉

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+	<nav class="py-5">
+		<ul class="flex justify-center space-x-4">
+			<li>
+				<router-link to="/" class="text-blue-500 hover:underline">Todos</router-link>
+			</li>
+			<li>
+				<router-link to="/about" class="text-blue-500 hover:underline">About</router-link>
+			</li>
+		</ul>
+	</nav>
+</template>
+
+<script setup lang="ts"></script>
